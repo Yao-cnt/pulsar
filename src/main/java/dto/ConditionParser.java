09440e3929b220b7a912eed1892f852ef30d4c0d
yaocheng liu <yaocheng.liu-cnt@lntecc.com>
2023-09-19 08:39:34 -0500

    pulsar function integrate with database

--- src/main/java/dto/ConditionParser.java
+++ src/main/java/dto/ConditionParser.java
@@ -10,4 +10,14 @@ public class ConditionParser {
     Integer measurementValue;
     String measurementName;
     boolean discrete;
+
+    @Override
+    public String toString() {
+        return "ConditionParser{" +
+                "conditionText='" + conditionText + '\'' +
+                ", measurementValue=" + measurementValue + '\'' +
+                ", measurementName='" + measurementName + '\'' +
+                ", discrete=" + discrete + '\'' +
+                '}';
+    }
 }
