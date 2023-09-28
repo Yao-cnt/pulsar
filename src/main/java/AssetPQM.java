5586a2a5178c74e6cc7aec98220a1d68c6556c7d
yaocheng liu <yaocheng.liu-cnt@lntecc.com>
2023-09-28 09:45:48 -0500

    clean up code and try with complex evaluate condition

--- src/main/java/AssetPQM.java
+++ src/main/java/AssetPQM.java
@@ -16,7 +16,6 @@ public class AssetPQM implements Function<String, String> {
 
         String assetId = context.getFunctionName();
         assetCondition.setAssetId(assetId);
-        StringBuilder asset = new StringBuilder("Asset Id: " + assetId).append("\n");
         MeasurementConditionListDTO measurementConditionListDTO = mapper.readValue(input, MeasurementConditionListDTO.class);
 
         List<ConditionParser> conditionParserList = measurementConditionListDTO.getConditionParserList();
@@ -24,9 +23,6 @@ public class AssetPQM implements Function<String, String> {
             Measurement measurement1 = new Measurement();
             measurement1.setMeasurementName(conditionParser.getMeasurementName());
             measurement1.setMeasurementValue(conditionParser.getMeasurementValue());
-            StringBuilder measurement = new StringBuilder("\n")
-                    .append("Measurement Name: ").append(conditionParser.getMeasurementName()).append("\n")
-                    .append("Measurement Value: ").append(conditionParser.getMeasurementValue()).append("\n");
             List<AlarmConditionDTO> alarmConditionDtoList = conditionParser.getAlarmConditionDtoList();
             List<AlarmCondition> alarmConditions = new ArrayList<>();
             alarmConditionDtoList.forEach(alarmConditionDTO -> {
@@ -36,17 +32,11 @@ public class AssetPQM implements Function<String, String> {
                 alarmCondition.setDiscrete(conditionParser.isDiscrete());
                 alarmCondition.setTriggered(trigger);
                 alarmConditions.add(alarmCondition);
-                measurement.append("Alarm Condition: ").append(alarmConditionDTO.getAlarmCondition()).append("\n")
-                        .append("If Discrete: ").append(conditionParser.isDiscrete()).append("\n")
-                        .append("If Triggered: ").append(trigger).append("\n");
             });
             measurement1.setAlarmConditions(alarmConditions);
-            asset.append(measurement);
             measurements.add(measurement1);
         });
         assetCondition.setMeasurements(measurements);
         return mapper.writeValueAsString(assetCondition);
-
-        //return asset.toString();
     }
 }
